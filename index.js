315897b6c0ec9c8f1e6ec82d78446d4b009af2e8
Dominik Mikuška <dominik.mikuska@gmail.com>
2023-01-05 00:18:29 +0100

    Added express for replit uptime

--- index.js
+++ index.js
@@ -110,3 +110,15 @@ client.on(Events.MessageCreate, (message) => {
 
 // Log in to Discord with your client's token
 client.login(tkn);
+
+//...Discord Bot Code Above ^^
+
+const express = require("express");
+const app = express();
+const port = 3000;
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
